e24ae2888866eac681c183ecbcc4138202a2660d
Koupah <ktayl21@yahoo.com.au>
2021-01-07 23:15:42 +1000

    Fix Login Issue
    Login issue occurs when setting the 'redirect_uri' property to the beta website

--- src/main/java/me/tewpingz/valorant/Valorant.java
+++ src/main/java/me/tewpingz/valorant/Valorant.java
@@ -37,7 +37,7 @@ class Valorant {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("client_id", "play-valorant-web-prod");
         jsonObject.addProperty("nonce", 1);
-        jsonObject.addProperty("redirect_uri", "https://beta.playvalorant.com/opt_in");
+        jsonObject.addProperty("redirect_uri", "https://playvalorant.com/opt_in");
         jsonObject.addProperty("response_type", "token id_token");
 
         this.post("https://auth.riotgames.com/api/v1/authorization", jsonObject.toString());
@@ -51,7 +51,7 @@ class Valorant {
         JsonObject responseObject = authResponse.get("response").getAsJsonObject();
         JsonObject parametersObject = responseObject.get("parameters").getAsJsonObject();
         String uri = String.valueOf(parametersObject.get("uri"));
-        String[] parts = uri.replace("https://beta.playvalorant.com/opt_in#", "").split("&");
+        String[] parts = uri.replace("https://playvalorant.com/opt_in#", "").split("&");
         String token = parts[0].replace("access_token=", "").replace("\"", "");
 
         ValHeader authHeader = new ValHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
